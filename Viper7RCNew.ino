2ae1db87af095fd5ed26b9302f779e40f0f353bf
David Andrus <Viper7171@outlook.com>
2023-03-17 15:36:13 -0400

    Update Viper7RCNew.ino

--- Viper7RCNew.ino
+++ Viper7RCNew.ino
@@ -64,7 +64,35 @@ int speedMin = 1000;
 int nSVal = 0;
 int nTVal = 0;
 
+////////////// PPM 
+/*
+ * PPM generator originally written by David Hasko
+ * on https://code.google.com/p/generate-ppm-signal/ 
+ */
 
+//////////////////////CONFIGURATION///////////////////////////////
+#define CHANNEL_NUMBER 8  //set the number of chanels
+#define CHANNEL_DEFAULT_VALUE 1500  //set the default servo value
+#define FRAME_LENGTH 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PULSE_LENGTH 300  //set the pulse length
+#define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 3  //set PPM signal output pin on the arduino
+//////////////////////////////////////////////////////////////////
+
+#define SWITCH_PIN 16
+#define CHANNEL_TO_MODIFY 11
+#define SWITCH_STEP 100
+
+byte previousSwitchValue;
+
+/*this array holds the servo values for the ppm signal
+ change these values in your code (usually servo values move between 1000 and 2000)*/
+int ppm[CHANNEL_NUMBER];
+
+int currentChannelStep;
+bool printAngle;
+uint8_t state = 0;
+/////////////
 
 
 
