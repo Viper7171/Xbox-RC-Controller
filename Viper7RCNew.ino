9a31923580684d385a45afbb0cfe70fe6f7af585
David Andrus <Viper7171@outlook.com>
2023-03-23 18:05:42 -0400

    Update Viper7RCNew.ino

--- Viper7RCNew.ino
+++ Viper7RCNew.ino
@@ -121,11 +121,11 @@ void setup() {
   Serial.begin(115200);
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
   if (Usb.Init() == -1) {
-    Serial.print(F("\r\nOSC did not start"));
+//    Serial.print(F("\r\nOSC did not start"));
     while (1); //halt
   }
-  Serial.print(F("\r\nXBOX ONE USB Library Started"));
-  Serial.println();
+//  Serial.print(F("\r\nXBOX ONE USB Library Started"));
+//  Serial.println();
 
  
  ///////////////////////////////
